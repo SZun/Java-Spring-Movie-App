99dfdfa20ee8dd2ec0ad22e87a099d375606406d
Zun <zunsamuel97@gmail.com>
2020-08-04 17:36:24 -0500

    added in new mothod for movie repo

--- server/src/main/java/com/sgz/server/repos/MovieRepo.java
+++ server/src/main/java/com/sgz/server/repos/MovieRepo.java
@@ -4,9 +4,12 @@ import com.sgz.server.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MovieRepo extends JpaRepository<Movie, UUID> {
+
+    List<Movie> findAllByGenre_Name(String genre);
 
 }
