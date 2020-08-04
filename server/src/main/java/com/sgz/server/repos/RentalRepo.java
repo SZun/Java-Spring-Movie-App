caa62a56b37b083ae137cd1ee1e6c00e5d2f03e8
Zun <zunsamuel97@gmail.com>
2020-08-04 00:42:22 -0500

    created repositories for the four new entities

---
+++ server/src/main/java/com/sgz/server/repos/RentalRepo.java
@@ -0,0 +1,11 @@
+package com.sgz.server.repos;
+
+import com.sgz.server.entities.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RentalRepo extends JpaRepository<Rental, UUID> {
+}
