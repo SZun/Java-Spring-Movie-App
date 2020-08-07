c3a2e616204b70ff86db30435d8c6d57c2533cf6
Zun <zunsamuel97@gmail.com>
2020-08-07 01:46:06 -0500

    generated genre test

---
+++ server/src/test/java/com/sgz/server/controllers/GenreControllerTest.java
@@ -0,0 +1,28 @@
+package com.sgz.server.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenreControllerTest {
+
+    @Test
+    void getAllGenres() {
+    }
+
+    @Test
+    void getGenreById() {
+    }
+
+    @Test
+    void createGenre() {
+    }
+
+    @Test
+    void updateGenre() {
+    }
+
+    @Test
+    void deleteGenreById() {
+    }
+}
