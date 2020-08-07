a615e599cd6e8bd88dae6bba6015094cddb3d4f6
Zun <zunsamuel97@gmail.com>
2020-08-06 21:20:07 -0500

    updated javax validation constraints for movie entity, implemented movie service, added in method into auth service for getting customers id by auth

--- server/src/main/java/com/sgz/server/services/AuthService.java
+++ server/src/main/java/com/sgz/server/services/AuthService.java
@@ -1,5 +1,6 @@
 package com.sgz.server.services;
 
+import com.sgz.server.repos.CustomerRepo;
 import com.sgz.server.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,15 +12,22 @@ import java.util.UUID;
 public class AuthService {
 
     private final UserRepo userRepo;
+    private final CustomerRepo customerRepo;
 
     @Autowired
-    public AuthService(UserRepo userRepo) {
+    public AuthService(UserRepo userRepo, CustomerRepo customerRepo) {
         this.userRepo = userRepo;
+        this.customerRepo = customerRepo;
     }
 
     public UUID getUserId() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepo.findByUsername(username).get().getId();
+    }
+
+    public UUID getCustomerId() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        return customerRepo.findByName(name).get().getId();
     }
 
 }
---
+++ server/src/main/java/com/sgz/server/services/MovieService.java
@@ -0,0 +1,89 @@
+package com.sgz.server.services;
+
+import com.sgz.server.entities.Movie;
+import com.sgz.server.exceptions.InvalidEntityException;
+import com.sgz.server.exceptions.InvalidIdException;
+import com.sgz.server.exceptions.NoItemsException;
+import com.sgz.server.repos.MovieRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class MovieService {
+
+    private final MovieRepo movieRepo;
+
+    @Autowired
+
+    public MovieService(MovieRepo movieRepo) {
+        this.movieRepo = movieRepo;
+    }
+
+    public List<Movie> getAllMovies() throws NoItemsException {
+        List<Movie> allMovies = movieRepo.findAll();
+
+        if(allMovies.isEmpty()) throw new NoItemsException("No Items");
+
+        return allMovies;
+    }
+
+    public List<Movie> getAllByGenreName(String name) throws InvalidEntityException, NoItemsException {
+        if(name == null || name.trim().length() < 5
+                || name.trim().length() > 50){
+            throw new InvalidEntityException("Invalid Name");
+        }
+
+        List<Movie> allMovies = movieRepo.findAllByGenre_Name(name);
+
+        if(allMovies.isEmpty()) throw new NoItemsException("No Items");
+
+        return allMovies;
+    }
+
+    public Movie getMovieById(UUID id) throws InvalidIdException {
+        Optional<Movie> toGet = movieRepo.findById(id);
+
+        if(!toGet.isPresent()) throw new InvalidIdException("Invalid Id");
+
+        return toGet.get();
+    }
+
+    public Movie createMovie(Movie toAdd) throws InvalidEntityException {
+        validateMovie(toAdd);
+
+        return movieRepo.save(toAdd);
+    }
+
+    public Movie updateMovie(Movie toEdit) throws InvalidEntityException, InvalidIdException {
+        validateMovie(toEdit);
+        checkExistsById(toEdit.getId());
+
+        return movieRepo.save(toEdit);
+    }
+
+    public void deleteMovieById(UUID id) throws InvalidIdException {
+        checkExistsById(id);
+
+        movieRepo.deleteById(id);
+    }
+
+    private void validateMovie(Movie toUpsert) throws InvalidEntityException {
+        if(toUpsert == null || toUpsert.getTitle().trim().length() < 5
+                            || toUpsert.getTitle().trim().length() > 255
+                            || toUpsert.getGenre() == null
+                            || toUpsert.getQty() > 99999
+                            || toUpsert.getQty() > 1
+                            || toUpsert.getDailyRate().compareTo(new BigDecimal("99.991")) == 1
+                            || toUpsert.getDailyRate().compareTo(new BigDecimal("00.00")) == -1)
+            throw new InvalidEntityException("Invalid Entity");
+    }
+
+    private void checkExistsById(UUID id) throws InvalidIdException {
+        if(!movieRepo.existsById(id)) throw new InvalidIdException("Invalid Id");
+    }
+}
