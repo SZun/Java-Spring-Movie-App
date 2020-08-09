83f48415c47a65328d0974773e2bf076dacb95c9
Zun <zunsamuel97@gmail.com>
2020-08-09 02:33:37 -0500

    seed file update

--- MoviesSeed.sql
+++ MoviesSeed.sql
@@ -7,4 +7,4 @@ SET @User_Role_Id  = (UUID_TO_BIN('2bd87c4e-39ac-41fd-854d-b480af2374e4', true))
 
 INSERT INTO Users(Id, Username, `Password`) VALUES(@Zun_Id, 'Zun','$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS');	
 INSERT INTO Roles(Id, Authority) VALUES(@User_Role_Id, 'USER'),(@Admin_Role_Id,'ADMIN'),(@Employee_Role_Id, 'EMPLOYEE');
-INSERT INTO Users_Roles(User_Id,Role_Id) VALUES(@Zun_Id,@Admin_Role_Id),(@Zun_Id,@User_Role_Id);
+INSERT INTO Users_Roles(User_Id,Role_Id) VALUES(@Zun_Id,@Admin_Role_Id),(@Zun_Id,@User_Role_Id),(@Zun_Id,@Employee_Role_Id);
